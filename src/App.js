a6a12ff553da03617f81512dd9876467f320e083
김우곤 <kwgon0102@gmail.com>
2023-12-20 22:58:27 +0900

    23년 12월 20일 map, useState 공부

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [글제목, b] = useState([
+  let [글제목, 글제목변경] = useState([
     "남자 코트 추천",
     "강남 우동맛집",
     "파이썬 독학",
   ]);
-  let [따봉, c] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>React Blog</h4>
       </div>
-      <div className="list">
+      {/* <div className="list">
         <h4>
           {글제목[0]}{" "}
           <span
             onClick={() => {
-              따봉++;
+              따봉변경(따봉++);
             }}
           >
             👍
@@ -29,15 +31,81 @@ function App() {
           {따봉}
         </h4>
         <p>2월 17일 발행</p>
+        <button
+          onClick={() => {
+            let copy = [...글제목];
+            copy[0] = "여자 코트 추천";
+            글제목변경(copy);
+          }}
+        >
+          글제목 변경
+        </button>
       </div>
       <div className="list">
         <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
-      <div className="list">
+      <div
+        className="list"
+        onClick={() => {
+          setModal(!modal);
+        }}
+      >
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {modal == true ? <Modal /> : null}
+
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        가나다순 정렬
+      </button>
+
+      {글제목.map((a, i) => {
+        return (
+          <div className="list" key={i}>
+            <h4>
+              {a}{" "}
+              <span
+                onClick={() => {
+                  let copy = [...따봉];
+                  copy[i] += 1;
+                  따봉변경(copy);
+                }}
+              >
+                👍
+              </span>{" "}
+              {따봉[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...글제목];
+                copy[0] = "여자 코트 추천";
+                글제목변경(copy);
+              }}
+            >
+              글제목 변경
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
